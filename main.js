1ddc0512057e0ed50788cef85e111121f96371e9
Rems008 <remivivien@lapos te.net>
2023-01-31 18:11:19 +0100

    v31/01

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const burgerToggler = document.querySelector(".icon-burger");
-const imgBurger = document.querySelector(".burger");
-const navContainer = document.querySelector(".nav-container");
+// const burgerToggler = document.querySelector(".icon-burger");
+// const imgBurger = document.querySelector(".burger");
+// const navContainer = document.querySelector(".nav-container");
 
-const toggleNav = (e) => {
-  burgerToggler.classList.toggle("open");
-  navContainer.classList.toggle("open");
-};
-burgerToggler.addEventListener("click", toggleNav);
-navContainer.addEventListener("mouseleave", function () {
-  navContainer.classList.remove("open");
+// const toggleNav = (e) => {
+//   burgerToggler.classList.toggle("open");
+//   navContainer.classList.toggle("open");
+// };
+// burgerToggler.addEventListener("click", toggleNav);
+// navContainer.addEventListener("mouseleave", function () {
+//   navContainer.classList.remove("open");
+// });
+// imgBurger.addEventListener("click", noMenu);
+const menuBurger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+
+menuBurger.addEventListener("click", () => {
+  navMenu.classList.toggle("mobile-menu");
 });
-imgBurger.addEventListener("click", noMenu);
