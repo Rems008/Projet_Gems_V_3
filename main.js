eae2cebfafd85a3843076f94bbc7cf8149eb0f1c
Rems008 <remivivien@lapos te.net>
2023-02-01 11:59:55 +0100

    v1/02H12H

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-// const burgerToggler = document.querySelector(".icon-burger");
-// const imgBurger = document.querySelector(".burger");
-// const navContainer = document.querySelector(".nav-container");
+// FUNCTION BANDEAU TEXTE
 
-// const toggleNav = (e) => {
-//   burgerToggler.classList.toggle("open");
-//   navContainer.classList.toggle("open");
-// };
-// burgerToggler.addEventListener("click", toggleNav);
-// navContainer.addEventListener("mouseleave", function () {
-//   navContainer.classList.remove("open");
-// });
-// imgBurger.addEventListener("click", noMenu);
+const bandeauText = document.querySelector(".bandeau-text");
+
+function bandeauChange() {
+  bandeauText.innerHTML = "RÈGLEMENT 3 FOIS SANS FRAIS DÉS 200 €";
+}
+setInterval("bandeauChange()", 5000);
+
+function bandeauChangeDeux() {
+  bandeauText.innerHTML =
+    "LIVRAISON OFFERTE À PARTIR DE 300 € EN FRANCE MÉTROPOLITAINE";
+}
+setInterval("bandeauChangeDeux()", 10000);
+
+// FUNCTION DU MENU
+
 const menuBurger = document.querySelector(".burger");
 const navMenu = document.querySelector(".nav-menu");
 
 menuBurger.addEventListener("click", () => {
   navMenu.classList.toggle("mobile-menu");
+});
+
+navMenu.addEventListener("mouseleave", () => {
+  navMenu.classList.remove("mobile-menu");
 });
